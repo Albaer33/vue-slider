19fdff1102e6759bf44c23d3850732c079ecb9af
Albaer33 <alisi3396@gmail.com>
2021-12-03 16:44:16 +0100

    standard part end

--- js/script.js
+++ js/script.js
@@ -4,35 +4,52 @@ const app = new Vue(
     {
         el: '#root',
         data: {
-            slide: [
+            currentImgCounter: 0,
+            slides: [
                 {
-                    image: 'img/01.jpg',
+                    img: 'img/01.jpg',
                     title: 'Svezia',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
                 },
                 {
-                    image: 'img/02.jpg',
+                    img: 'img/02.jpg',
                     title: 'Svizzera',
                     text: 'Lorem ipsum'
                 },
                 {
-                    image: 'img/03.jpg',
+                    img: 'img/03.jpg',
                     title: 'Gran Bretagna',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
                 },
                 {
-                    image: 'img/04.jpg',
+                    img: 'img/04.jpg',
                     title: 'Germania',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
                 },
                 {
-                    image: 'img/05.jpg',
+                    img: 'img/05.jpg',
                     title: 'Paradise',
                     text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
                 }
             ]
         },
         methods: {
+            nextSlide: function() {
+                if(this.currentImgCounter < this.slides.length-1){
+                    this.currentImgCounter++;
+                }
+                else {
+                    this.currentImgCounter = 0;
+                }
+            },
+            previousSlide: function() {
+                if(this.currentImgCounter > 0) {
+                    this.currentImgCounter--;
+                }
+                else {
+                    this.currentImgCounter = this.slides.length-1;
+                }
+            }
         }
     }
 );
